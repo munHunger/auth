1651058fbae5089155bdcc9f1856fe8e1a888db8
munHunger <marcusmunger@gmail.com>
2021-05-31 14:58:13 +0200

    Update logger.js

--- server/src/lib/logger.js
+++ server/src/lib/logger.js
@@ -8,6 +8,8 @@ const options = {
 		// - Write all logs with level `error` and below to `error.log`
 		// - Write all logs with level `info` and below to `combined.log`
 		//
+		new winston.transports.File({ filename: '/tmp/log/auth-error.log', level: 'error' }),
+		new winston.transports.File({ filename: '/tmp/log/auth-combined.log' }),
 		new winston.transports.Console({
 			level: 'info',
 			format: winston.format.combine(winston.format.colorize(), winston.format.simple())
